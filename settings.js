e103214cc1065ba9032e72791e99d00d026104d5
gasantha646 <hondaginoj@gmail.com>
2025-06-15 13:02:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "𝚁𝙰𝚅𝙸𝙽𝙳𝚄-𝙼𝙳=ugxUSLTS#uwiieBS8-GMYIqGPHW76ATqdZQZYW8cDOWxofOCd5lE",// session id 💙
+HACKER : process.env.HACKER || "94722899613" //Addmin number 💙
 };
